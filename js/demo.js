16d1285eb0466a121f209811b294a393c21228eb
Georgiana <georgiana@reanimate.tech>
2017-12-31 13:07:42 +0200

    upload all selected files

--- js/demo.js
+++ js/demo.js
@@ -2,8 +2,8 @@
 var credentialsUrl = 'http://localhost:5000/s3_credentials';
 
 $('.upload-btn').on('click', function (){
-    $('.progress-bar').text('0%');
-    $('.progress-bar').width('0%');
+  $('.progress-bar').text('0%');
+  $('.progress-bar').width('0%');
 });
 
 $('#upload-input').on('change', function(){
@@ -24,72 +24,71 @@ $('#uploadVideo').submit(function(){
       var file = files[i];
       // add the files to formData object for the data payload
       formData.append('uploads[]', file, file.name);
+      uploadFileToS3(file);
     }
-
-    var file = files[0];
-    var filename = file.name;
-    var contentType = file.type;
-
-    $.ajax({
-      url: credentialsUrl,
-      type: 'GET',
-      dataType: 'json',
-      data: {
-        filename: filename,
-        content_type: contentType
-      },
-      success: function(s3Data) {
-        var data = new FormData();
-
-        Object.keys(s3Data.params).forEach(function(key, idx) {
-          console.log(key, idx, s3Data.params[key]);
-          data.append(key, s3Data.params[key]);
-        });
-        data.append('file', file, filename);
-
-        var url = s3Data.endpoint_url;
-
-        $.ajax({
-          url: url,
-          type: 'POST',
-          data: data,
-          processData: false,
-          contentType: false,
-          success: function(data){
-              console.log('upload successful!\n' + data);
-          },
-          xhr: function() {
-            // create an XMLHttpRequest
-            var xhr = new XMLHttpRequest();
-
-            // listen to the 'progress' event
-            xhr.upload.addEventListener('progress', function(evt) {
-
-              if (evt.lengthComputable) {
-                // calculate the percentage of upload completed
-                var percentComplete = evt.loaded / evt.total;
-                percentComplete = parseInt(percentComplete * 100);
-
-                // update the Bootstrap progress bar with the new percentage
-                $('.progress-bar').text(percentComplete + '%');
-                $('.progress-bar').width(percentComplete + '%');
-
-                // once the upload reaches 100%, set the progress bar text to done
-                if (percentComplete === 100) {
-                  $('.progress-bar').html('Done');
-                }
-              }
-
-            }, false);
-
-            return xhr;
-          }
-        });
-      }
-    });
 
   } else {
     $('#require-selection').text("Please choose a video.");
   }
   return false;
 });
+
+function uploadFileToS3(file) {
+  $.ajax({
+    url: credentialsUrl,
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      filename: file.name,
+      content_type: file.type
+    },
+
+    success: function(s3Data) {
+      var data = new FormData();
+
+      Object.keys(s3Data.params).forEach(function(key, idx) {
+        data.append(key, s3Data.params[key]);
+      });
+      data.append('file', file, file.name);
+
+      var url = s3Data.endpoint_url;
+
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function(data){
+          console.log('upload successful!\n' + data);
+        },
+        xhr: function() {
+          // create an XMLHttpRequest
+          var xhr = new XMLHttpRequest();
+
+          // listen to the 'progress' event
+          xhr.upload.addEventListener('progress', function(evt) {
+
+            if (evt.lengthComputable) {
+              // calculate the percentage of upload completed
+              var percentComplete = evt.loaded / evt.total;
+              percentComplete = parseInt(percentComplete * 100);
+
+              // update the Bootstrap progress bar with the new percentage
+              $('.progress-bar').text(percentComplete + '%');
+              $('.progress-bar').width(percentComplete + '%');
+
+              // once the upload reaches 100%, set the progress bar text to done
+              if (percentComplete === 100) {
+                $('.progress-bar').html('Done');
+              }
+            }
+
+          }, false);
+
+          return xhr;
+        }
+      });
+    }
+  });
+}
